c6a78c484589c4f0b6a7e35a0a64402b96250785
Su Bae <tugisuwon@gmail.com>
2023-05-26 22:32:33 -0400

    Update tutor_signup.py

--- pages/tutor_signup.py
+++ pages/tutor_signup.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
+#from streamlit_pandas_profiling import st_profile_report
 import pandas as pd
 import pandas_profiling
 from PIL import Image
