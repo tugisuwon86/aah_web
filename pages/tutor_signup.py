4eab92d467c1714b40be6d8ecf9da665dff07d84
Su Bae <tugisuwon@gmail.com>
2023-07-11 00:12:31 -0400

    Update tutor_signup.py
    commenting unused calendar view

--- pages/tutor_signup.py
+++ pages/tutor_signup.py
@@ -66,33 +66,33 @@ tutor = meta_col2.selectbox('Tutor', tutor_option)
 # ---------------------------------------------------------------------------------------------------------
 
 ## calendar view
-from calendar_view.core import data
-from calendar_view.core.config import CalendarConfig
-from calendar_view.calendar import Calendar
-from calendar_view.core.event import Event, EventStyles
+# from calendar_view.core import data
+# from calendar_view.core.config import CalendarConfig
+# from calendar_view.calendar import Calendar
+# from calendar_view.core.event import Event, EventStyles
 
-config = data.CalendarConfig(
-    lang='en',
-    title=tutor,
-    dates='',
-    show_year=True,
-    legend=False,
-)
-events = [
-    Event('Planning', day='2019-09-23', start='11:00', end='13:00'),
-    Event('Demo', day='2019-09-27', start='15:00', end='16:00'),
-    Event('Retrospective', day='2019-09-27', start='17:00', end='18:00'),
-]
+# config = data.CalendarConfig(
+#     lang='en',
+#     title=tutor,
+#     dates='',
+#     show_year=True,
+#     legend=False,
+# )
+# events = [
+#     Event('Planning', day='2019-09-23', start='11:00', end='13:00'),
+#     Event('Demo', day='2019-09-27', start='15:00', end='16:00'),
+#     Event('Retrospective', day='2019-09-27', start='17:00', end='18:00'),
+# ]
 
-data.validate_config(config)
-data.validate_events(events, config)
+# data.validate_config(config)
+# data.validate_events(events, config)
 
-calendar = Calendar.build(config)
-calendar.add_events(events)
-calendar.save("yoga_class.png")
+# calendar = Calendar.build(config)
+# calendar.add_events(events)
+# calendar.save("yoga_class.png")
 
-image = Image.open("yoga_class.png")
-st.image(image, caption='calendar')
+# image = Image.open("yoga_class.png")
+# st.image(image, caption='calendar')
 
 
 temp = df.loc[(df['Name']==tutor) & (df['Schedule'].str.contains(weekday_mapping[weekday]))]
