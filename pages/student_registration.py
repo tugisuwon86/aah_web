df91a744637a1b7365181c464ad9598cfa59ab39
Su Bae <tugisuwon@gmail.com>
2023-07-11 00:25:12 -0400

    Create student_registration.py
    added student registration page

---
+++ pages/student_registration.py
@@ -0,0 +1,62 @@
+import streamlit as st
+#from streamlit_pandas_profiling import st_profile_report
+import pandas as pd
+import pandas_profiling
+import re
+
+st.markdown("""
+    <style>
+        .stMultiSelect [data-baseweb=select] span{
+            max-width: 250px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+
+st.title('AAH Student Registration')
+
+# ---------------------------------------------------------------------------------------------------------
+meta_col0, meta_col1, meta_col2 = st.columns(3)
+
+# ---------------------------------------------------------------------------------------------------------
+# Read data from google sheets to initiate
+import gspread
+
+credentials = st.secrets['gcp_service_account']
+sa = gspread.service_account_from_dict(credentials)
+sh = sa.open("AAh schedules")
+
+wks_student = sh.worksheet("Students_Registration")
+
+# read google sheets as dataframe
+df_student = pd.DataFrame(wks_student.get_all_records())
+st.dataframe(df_student)
+
+with st.form('tutor_registration_form'):
+  first_name = st.text_input('Your first name')
+  last_name = st.text_input('Your last name')
+  email = st.text_input('Please type your email - must provide valid email; otherwise, the registration will be rejected')
+
+  grade = st.slider('Your grade', 5, 12, 5)
+  country = st.text_input('Your country')
+  referral = st.text_input('How did you hear about us?')
+
+  school = st.text_input('Your school')
+
+  submitted = st.form_submit_button("Submit tutor registration form")
+  if submitted:
+    # first check valid email
+    if first_name.strip() == '' or last_name.strip() == '':
+      st.error('please provide your full name', icon="🚨")
+    elif not re.fullmatch(r"[^@]+@[^@]+\.[^@]+", email):
+      st.error('please provide valid email address', icon="🚨")
+    elif email.strip() in df_tutor['email'].values:
+      st.error('you are already registered!', icon="🚨")
+    else:
+      df_student.loc[len(df_student.index)] = [first_name, last_name, email, grade, country, referral, math_subjects, eng_subjects, 'N']
+      st.dataframe(df_student)
+      wks_student.update([df_student.columns.values.tolist()] + df_student.values.tolist())
+      st.write("We received your registration! Please give us 24 hours to approve your registration!")
+
+  
+
