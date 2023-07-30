46c15379f153a9e4b224d28cbcebd1fb98c698f4
Su Bae <tugisuwon@gmail.com>
2023-07-30 08:13:39 -0400

    Update tutor_signup.py

--- pages/tutor_signup.py
+++ pages/tutor_signup.py
@@ -6,15 +6,22 @@ from datetime import datetime
 import datetime as dt
 import pytz
 
-st.markdown("""
-    <style>
-        .stMultiSelect [data-baseweb=select] span{
-            max-width: 250px;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+st.set_page_config(page_title="Tutor Sign up Form", page_icon="📈")
 
-st.title('AAH Tutor Scheduler')
+from st_pages import Page, Section, show_pages, add_page_title
+show_pages(
+    [
+        Page("main.py", "Home", "🏠"),
+        Section(name="For Tutors Only", icon="🎈️"),
+        Page("pages/tutor_registration.py", "Tutor Registration"),
+        Page("pages/tutor_availability.py", "Tutor Availability Update"),
+        
+        Section(name="For Students Only", icon="💪"),    
+        Page("pages/student_registration.py", "Student Registration"),
+        Page("pages/tutor_signup.py", "Tutor Sign Up"),
+    ]
+)
+add_page_title() # By default this also adds indentation
 
 meta_col0, meta_col1, meta_col2 = st.columns(3)
 # ---------------------------------------------------------------------------------------------------------
