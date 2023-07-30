1e228a06b4085a8dbc0891bf64a397b0f9c5d1ce
Su Bae <tugisuwon@gmail.com>
2023-07-30 07:58:41 -0400

    Update tutor_signup.py

--- pages/tutor_signup.py
+++ pages/tutor_signup.py
@@ -56,7 +56,8 @@ st.write('Your status summary---------')
 # make sure the student is in our system
 check_ = df_student[(df_student['email'] == email) & (df_student['complete'] == 'Y')]
 number_of_booking = df[df['Student Email'] == email]
-print(email, number_of_booking.head())
+print(df.head())
+print(email, number_of_booking.shape())
 if check_.shape[0] == 0:
     st.error('Your email address is not found in our system. Please register from the main website first', icon="🚨")
 elif number_of_booking.shape[0] >= 2:
