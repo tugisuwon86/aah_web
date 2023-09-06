4c9e9a470938a338488f9cb0484f1b550eddcebd
Su Bae <tugisuwon@gmail.com>
2023-09-06 19:05:26 -0400

    Update summary.py

--- pages/summary.py
+++ pages/summary.py
@@ -1,1 +1,37 @@
-#
+import streamlit as st
+#from streamlit_pandas_profiling import st_profile_report
+import pandas as pd
+import pandas_profiling
+import re
+import json
+
+st.markdown("""
+    <style>
+        .stMultiSelect [data-baseweb=select] span{
+            max-width: 250px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+
+# st.title('AAH Tutor Registration')
+
+from st_pages import Page, Section, show_pages, add_page_title
+show_pages(
+    [
+        Page("main.py", "Home", "🏠"),
+        Section(name="For Tutors Only", icon="🎈️"),
+        Page("pages/tutor_registration.py", "Tutor Registration"),
+        Page("pages/tutor_availability.py", "Tutor Availability Update"),
+        
+        Section(name="For Students Only", icon="💪"),    
+        Page("pages/student_registration.py", "Student Registration"),
+        Page("pages/tutor_signup.py", "Tutor Sign Up"),
+
+        Section(name="Class Update", icon="💪"),    
+        # Page("pages/cancel_session.py", "Student Registration"),
+        Page("pages/complete_session.py", "Follow up"),
+    ]
+)
+add_page_title() # By default this also adds indentation
+
