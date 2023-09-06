76f0ff9871ec7d964676cb115aa16f25472c3c6f
Su Bae <tugisuwon@gmail.com>
2023-09-06 19:03:33 -0400

    Update tutor_signup.py

--- pages/tutor_signup.py
+++ pages/tutor_signup.py
@@ -141,6 +141,29 @@ check_ = df_student[(df_student['email'] == email) & (df_student['complete'] == 
 #print(df.head())
 
 def mailing(tutor, subject, email_, tutor_time, tutor_date, email):
+    import smtplib
+
+    # Import the email modules we'll need
+    from email.mime.text import MIMEText
+    
+    # Open a plain text file for reading.  For this example, assume that
+    # the text file contains only ASCII characters.
+    with open(textfile, 'rb') as fp:
+        # Create a text/plain message
+        msg = MIMEText(fp.read())
+    
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'The contents of %s' % textfile
+    msg['From'] = me
+    msg['To'] = you
+    
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, [you], msg.as_string())
+    s.quit()
+    
     import yagmail
     GMAIL = yagmail.SMTP("aahtutoringscheduler@gmail.com", "@RQu&S56pAS1")
 
