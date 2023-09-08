bb2b4b9a350fa6629e72ae35e5e1dba900d94492
Su Bae <tugisuwon@gmail.com>
2023-09-07 21:36:27 -0400

    Update tutor_availability.py

--- pages/tutor_availability.py
+++ pages/tutor_availability.py
@@ -16,7 +16,12 @@ show_pages(
         
         Section(name="For Students Only", icon="💪"),    
         Page("pages/student_registration.py", "Student Registration"),
-        Page("pages/tutor_signup.py", "Tutor Sign Up"),
+        Page("pages/tutor_signup.py", "Sing up for a session"),
+
+        Section(name="Class Update", icon="💪"),    
+        # Page("pages/cancel_session.py", "Student Registration"),
+        Page("pages/complete_session.py", "Follow up"),
+        Page("pages/summary.py", "Summary"),
     ]
 )
 add_page_title() # By default this also adds indentation
