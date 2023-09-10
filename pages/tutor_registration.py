1b564e6b5a2056ac6a6e308bcf34cc9ffe1ff9fd
Su Bae <tugisuwon@gmail.com>
2023-09-10 00:16:02 -0400

    Update tutor_registration.py

--- pages/tutor_registration.py
+++ pages/tutor_registration.py
@@ -87,6 +87,35 @@ with st.form('tutor_registration_form'):
   st.divider()  # 👈 Draws a horizontal rule
   st.write("Please send an email to freetutoring@americanassimilationhelpline.org with photo ID to complete the registration!")
   st.write("Tutors need to provide 1) Student ID or 2) Driver license - if you don't have one, please send us your parent's driver license to complete your registration")
+
+  st.markdown("**PLEASE READ BELOW BEFORE SUBMITTING YOUR APPLICATION**")
+  para = '''
+- We are using Google Meet for our online sessions (You need to have a Gmail account)
+1) go to meet.Google.com
+2) create an instant meeting
+3) click on the i in a circle and copy the info
+4) paste it in the email to your student
+
+-Tutor can sign up for a session whenever you are available. There is no minimum or maximum number of sessions required for volunteers.
+
+-Tutor must turn on your camera while you are in your session. 
+
+-Tutor can find our teaching materials for your session from our website. 
+For academic resources: https://www.americanassimilationhelpline.org/get_involved
+For Computer Science resources: 
+https://www.americanassimilationhelpline.org/coding
+Your student might bring their own material they want to work on. 
+
+-If you are tutoring English conversation, use or download Google Translate to translate English to your student's language - this allows for effective communication with your student. (for instance, Brazil's primary language is Portuguese)
+
+-Take a screenshot during each session with you and your student and email us at Freetutoring@americanassimilationhelpline.org. We use this to confirm your session took place for your volunteer hours. Take the picture at the beginning of your session to ensure you don't forget.
+
+-You need to download WhatsApp! We communicate through it. Every registered tutor will be added to one of our WhatsApp tutor group chats. 
+
+-Record your volunteer hours to confirm them with us. 
+  '''
+  st.markdown(para)
+    
   sent = st.checkbox('Sent email')
     
   submitted = st.form_submit_button("Submit tutor registration form")
